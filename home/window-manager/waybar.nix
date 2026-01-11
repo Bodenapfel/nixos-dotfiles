e3d811f5cd6901345020641b0952c6e0aa781345
Bodenapfel <dakorb20@gmail.com>
2026-01-11 12:44:14 +0100

    swaynd asdfoibna;sdlbnfo ;

--- home/window-manager/waybar.nix
+++ home/window-manager/waybar.nix
@@ -274,30 +274,6 @@ in
     comicShannsNerd
   ];
 
-  # User-level notification daemon + swaync-client
-  services.swaync = {
-    enable = true;
-    settings = {
-      positionX = "right";
-      positionY = "top";
-      layer = "overlay";
-      control-center-layer = "top";
-      layer-shell = true;
-      cssPriority = "application";
-      control-center-margin-top = 0;
-      control-center-margin-bottom = 0;
-      control-center-margin-right = 0;
-      control-center-margin-left = 0;
-      notification-2fa-action = true;
-      notification-inline-replies = false;
-      notification-icon-size = 64;
-      notification-body-image-height = 100;
-      notification-body-image-width = 200;
-    }
-    style = {
-      }
-  };
-
   # User fontconfig (so the font is picked up in your session)
   fonts.fontconfig.enable = true;
 }

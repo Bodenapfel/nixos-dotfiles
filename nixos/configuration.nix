675340bf1e6863c8c4a429bb275934f2fb8e1f57
Bodenapfel <dakorb20@gmail.com>
2026-01-12 22:43:18 +0100

    fastfetch fix and updated fonts config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -130,7 +130,7 @@
   nixpkgs.config.allowUnfreePredicate = pkg: 
     builtins.elem (lib.getName pkg) [ "corefonts" ];
 
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     corefonts
   ];
 

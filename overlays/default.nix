bbd41c7e6cf819aa7587a4d677d32c2da591c0e8
Bodenapfel <dakorb20@gmail.com>
2026-06-08 16:00:17 +0200

    Disable lager overlay workaround

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,20 +6,20 @@
   ...
 }:
 {
-  # weird cmake bug
   nixpkgs.overlays = [
     claude-code.overlays.default
     codex-cli.overlays.default
     (final: prev: {
 
-      lager = prev.lager.overrideAttrs (old: {
-        cmakeFlags = (old.cmakeFlags or [ ]) ++ [
-          "-Dlager_BUILD_TESTS=OFF"
-          "-Dlager_BUILD_FAILURE_TESTS=OFF"
-          "-Dlager_BUILD_DOCS=OFF"
-          "-Dlager_BUILD_DEBUGGER_EXAMPLES=OFF"
-        ];
-      });
+      # weird cmake bug
+      # lager = prev.lager.overrideAttrs (old: {
+      #   cmakeFlags = (old.cmakeFlags or [ ]) ++ [
+      #     "-Dlager_BUILD_TESTS=OFF"
+      #     "-Dlager_BUILD_FAILURE_TESTS=OFF"
+      #     "-Dlager_BUILD_DOCS=OFF"
+      #     "-Dlager_BUILD_DEBUGGER_EXAMPLES=OFF"
+      #   ];
+      # });
     })
   ];
 }

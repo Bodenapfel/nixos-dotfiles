65565f6aee315490c8b76d55f703389e5a9c14e6
Bodenapfel <dakorb20@gmail.com>
2026-07-06 22:10:22 +0200

    changed dns servers finally im free of the 10 second breaks fucking decades old fritz router grr

--- nixos/modules/misc/network.nix
+++ nixos/modules/misc/network.nix
@@ -9,6 +9,12 @@
     networking.hostName = "nixbtw"; # Define your hostname.
     # Configure network connections interactively with nmcli or nmtui.
     networking.networkmanager.enable = true;
+    networking.networkmanager.insertNameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
 
     # for vpn
     networking.firewall.checkReversePath = false;
